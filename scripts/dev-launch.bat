757c68a990fa9db866d5016f486fcf2c5dba17a4
Narayan Bandodker <narayanband1356@gmail.com>
2024-08-22 20:03:42 +0530

    Use consistent arguments passing

--- scripts/dev-launch.bat
+++ scripts/dev-launch.bat
@@ -2,4 +2,4 @@
 
 @echo off
 
-python3 -m uvicorn --factory --reload --host 0.0.0.0 --log-config=config/debug-logger.yml --timeout-graceful-shutdown=10 sample_fastapi:init_app
+python3 -m uvicorn --factory --reload --host=0.0.0.0 --log-config=config/debug-logger.yml --timeout-graceful-shutdown=10 sample_fastapi:init_app
