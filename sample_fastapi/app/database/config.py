bcb7f420bbc37a37be86a1f46c5076830809aef5
Narayan Bandodker <narayanband1356@gmail.com>
2024-11-03 16:26:47 +0530

    Default DSN use LRU cache for optimization and to make warning come up only once

--- sample_fastapi/app/database/config.py
+++ sample_fastapi/app/database/config.py
@@ -1,4 +1,5 @@
 import logging
+from functools import lru_cache
 
 from pydantic import Field
 
@@ -9,8 +10,11 @@ LOG = logging.getLogger(__name__)
 APP_DB_SESSION_MANAGER_KEY = "_dbsm"  # Lol
 
 
+@lru_cache()
 def _default_db_dsn():
-    LOG.warning("Using SQLite In-memory database as `database_url` config was not specified. Content WILL be lost after shutting down the app!")
+    LOG.warning(
+        "Using SQLite In-memory database as `database_url` config was not specified. Content WILL be lost after shutting down the app!"
+    )
     return "sqlite+aiosqlite:///:memory:"
 
 
